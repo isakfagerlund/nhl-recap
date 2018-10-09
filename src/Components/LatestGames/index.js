85b5fbecf78be3546dd4410c50446a09a8ff5c47
Michal Chmura <michalchmura@gmail.com>
2018-10-09 11:59:40 +0200

    Code refactor

--- src/Components/LatestGames/index.js
+++ src/Components/LatestGames/index.js
@@ -1,30 +1,31 @@
 import React, { Component } from 'react';
-import Game from './Game'
-import styled from 'styled-components'
+import axios from 'axios';
+import Game from './Game';
+import styled from 'styled-components';
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-`
+`;
 
 class LatestGames extends Component {
+  state = {
+    videos: [],
+    loaded: false,
+  };
 
-  constructor() {
-    super();
-    this.state = { videos: [], loaded: false };
-  }
+  componentDidMount = async () => {
+    const { data: { items } } = await axios.get('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=PL1NbHSfosBuHInmjsLcBuqeSV256FqlOO&key=AIzaSyDFlX0LLCc1b2cZG8aBM0BoN4a8aOq6hMQ');
 
-  componentDidMount() {
-    fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=PL1NbHSfosBuHInmjsLcBuqeSV256FqlOO&key=AIzaSyDFlX0LLCc1b2cZG8aBM0BoN4a8aOq6hMQ`)
-      .then(res => res.json())
-      .then(json => this.setState({ videos: json.items, loaded: true }));
+    this.setState({ videos: items, loaded: true });
   }
 
   render() {
+    const { videos } = this.state;
     return (
       <Container>
-        {this.state.videos.map(item => <Game key={item.id} id={item.id} title={item.snippet.title} thumbnail={item.snippet.thumbnails.high.url} videoId={item.snippet.resourceId.videoId} />)}
+        {videos.map(item => <Game key={item.id} id={item.id} title={item.snippet.title} thumbnail={item.snippet.thumbnails.high.url} videoId={item.snippet.resourceId.videoId} />)}
       </Container>
     );
   }
