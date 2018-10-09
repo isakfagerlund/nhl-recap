4598fc473bcb3830877b8e31aa699befa960cb6a
Michal Chmura <michalchmura@gmail.com>
2018-10-09 12:37:01 +0200

    Add team helper with team name dictionaries

---
+++ src/helpers/teams.js
@@ -0,0 +1,159 @@
+const teams = [
+  {
+    id: 1,
+    name: 'New Jersey Devils',
+    logo: '',
+  },
+  {
+    id: 2,
+    name: 'New York Islanders',
+    logo: '',
+  },
+  {
+    id: 3,
+    name: 'New York Rangers',
+    logo: '',
+  },
+  {
+    id: 4,
+    name: 'Philadelphia Flyers',
+    logo: '',
+  },
+  {
+    id: 5,
+    name: 'Pittsburgh Penguins',
+    logo: '',
+  },
+  {
+    id: 6,
+    name: 'Boston Bruins',
+    logo: '',
+  },
+  {
+    id: 7,
+    name: 'Buffalo Sabres',
+    logo: '',
+  },
+  {
+    id: 8,
+    name: 'Montréal Canadiens',
+    logo: '',
+  },
+  {
+    id: 9,
+    name: 'Ottawa Senators',
+    logo: '',
+  },
+  {
+    id: 10,
+    name: 'Toronto Maple Leafs',
+    logo: '',
+  },
+  {
+    id: 12,
+    name: 'Carolina Hurricanes',
+    logo: '',
+  },
+  {
+    id: 13,
+    name: 'Florida Panthers',
+    logo: '',
+  },
+  {
+    id: 14,
+    name: 'Tampa Bay Lightning',
+    logo: '',
+  },
+  {
+    id: 15,
+    name: 'Washington Capitals',
+    logo: '',
+  },
+  {
+    id: 16,
+    name: 'Chicago Blackhawks',
+    logo: '',
+  },
+  {
+    id: 17,
+    name: 'Detroit Red Wings',
+    logo: '',
+  },
+  {
+    id: 18,
+    name: 'Nashville Predators',
+    logo: '',
+  },
+  {
+    id: 19,
+    name: 'St. Louis Blues',
+    logo: '',
+  },
+  {
+    id: 20,
+    name: 'Calgary Flames',
+    logo: '',
+  },
+  {
+    id: 21,
+    name: 'Colorado Avalanche',
+    logo: '',
+  },
+  {
+    id: 22,
+    name: 'Edmonton Oilers',
+    logo: '',
+  },
+  {
+    id: 23,
+    name: 'Vancouver Canucks',
+    logo: '',
+  },
+  {
+    id: 24,
+    name: 'Anaheim Ducks',
+    logo: '',
+  },
+  {
+    id: 25,
+    name: 'Dallas Stars',
+    logo: '',
+  },
+  {
+    id: 26,
+    name: 'Los Angeles Kings',
+    logo: '',
+  },
+  {
+    id: 28,
+    name: 'San Jose Sharks',
+    logo: '',
+  },
+  {
+    id: 29,
+    name: 'Columbus Blue Jackets',
+    logo: '',
+  },
+  {
+    id: 30,
+    name: 'Minnesota Wild',
+    logo: '',
+  },
+  {
+    id: 52,
+    name: 'Winnipeg Jets',
+    logo: '',
+  },
+  {
+    id: 53,
+    name: 'Arizona Coyotes',
+    logo: '',
+  },
+  {
+    id: 54,
+    name: 'Vegas Golden Knights',
+    logo: '',
+  },
+];
+
+module.exports = { teams };
